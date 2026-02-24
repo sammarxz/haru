68d16b2a284114f02482b9ea61e9623923991ebe
Sam Marxz <sammarxzz@gmail.com>
2026-02-24 11:43:42 -0300

    fix ci issues: remove invalid regex options and align database port

--- config/dev.exs
+++ config/dev.exs
@@ -58,12 +58,12 @@ config :haru_web, HaruWebWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
+      ~r"priv/gettext/.*\.po$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/haru_web_web/router\.ex$"E,
-      ~r"lib/haru_web_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"lib/haru_web_web/router\.ex$",
+      ~r"lib/haru_web_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
